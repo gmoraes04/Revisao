a3c37cf45dcb296cbb7a9eea19f46ec4febd804b
Gabriel Moraes <gabrielmoraessilva0403@gmail.com>
2025-04-14 20:36:29 -0300

    inicio do projeto em Java

---
+++ calculadorav5/src/main/java/com/mycompany/calculadorav5/Calculadorav5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorav5;
+
+/**
+ *
+ * @author 08175
+ */
+public class Calculadorav5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
